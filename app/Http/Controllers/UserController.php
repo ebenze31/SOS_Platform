ae1131c8671102afdb5cf0431474556be36dada3
EBenze <52275898+ebenze31@users.noreply.github.com>
2026-03-29 14:36:19 +0700

    Update UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -180,14 +180,16 @@ class UserController extends Controller
 
         $user->update($data);
 
-        // อัปเดตข้อมูลรถ
-        DB::table('user_officers')
-            ->updateOrInsert(
-                ['user_id' => $id],
-                [
-                    'vehicle_type' => $request->vehicle_type,
-                ]
-            );
+        if($user->role == "officer"){
+            // อัปเดตข้อมูลรถ
+            DB::table('user_officers')
+                ->updateOrInsert(
+                    ['user_id' => $id],
+                    [
+                        'vehicle_type' => $request->vehicle_type,
+                    ]
+                );
+        }
 
         return redirect()->back()->with('success', 'อัปเดตสำเร็จ');
     }
